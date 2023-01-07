a55ecd00b5313da98dc9fec85bb3c132441143e1
saranshh <saranshchaurasia9@gmail.com>
2023-01-07 14:03:11 +0530

    Updates

---
+++ precache-manifest.a0a62fa30f60af8eef06acbb4f2bfc19.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d43e586246d21f557f67528dc23f82",
+    "url": "/toDoList-app/index.html"
+  },
+  {
+    "revision": "03413e1a4e0336fed639",
+    "url": "/toDoList-app/static/js/2.587271f8.chunk.js"
+  },
+  {
+    "revision": "aacf34d53911a24babca",
+    "url": "/toDoList-app/static/js/main.94661f95.chunk.js"
+  },
+  {
+    "revision": "bc241dce57551643312c",
+    "url": "/toDoList-app/static/js/runtime-main.6f7b4e25.js"
+  }
+]);
